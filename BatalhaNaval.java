20f6ce1b0b65a85cb59b81aa4b197661a457cefc
GuilhermeBolfe <guilhermebolfe11@gmail.com>
2016-06-11 17:58:50 -0300

    Fiz a tabela unicial, contador de quantas vezes tentou, as embarcações acertadas (Hrido e Sub)

--- BatalhaNaval.java
+++ BatalhaNaval.java
@@ -26,6 +26,8 @@ public static void main(String[] args){
 	boolean sair = false;
 	Double random1, random2, random3;
 	int posicao, navioY, navioX;
+        
+        
 	String[] navios	= {"A","A","A","A","A","S","S","S","S","CC","CC","CC","EEE","EEE","PPPP"};
 	int navioTotal = navios.length-1;
 	char[][] tabuleiro = new char[10][10];
@@ -75,21 +77,45 @@ public static void main(String[] args){
 	}
 	
 	char[][] nAcertados = new char[10][10];
-
-	while(sair==false){
+        int vezes =0;
+        int Hidroavioes=0, Submarinos=0;
+        
+	System.out.println("O jogo contem:\n");
+        System.out.println("|Embarcação  |Quantidade|Formato|");
+        System.out.println("|Hidroaviões |         5|      A|");
+        System.out.println("|Submarinos  |         4|      S|");
+        System.out.println("|Cruzadores  |         3|     CC|");
+        System.out.println("|Encouraçados|         2|    EEE|");
+        System.out.println("|Porta-aviões|         1|   PPPP|\n");
+        
+        while(sair==false){
 		Tiros(nAcertados);
 		System.out.println("Digite as coordenadas do ataque no formato x y. Ex.: 5 3");
 		String tiro = entrada.nextLine();
 		if(tiro.equals("0")){
 			sair = true;
 		}else{
-			if(tiro.matches("\\d \\d") || tiro.matches("\\d{2} \\d{2}") || tiro.matches("\\d \\d{2}") || tiro.matches("\\d{2} \\d")){	
+			vezes ++;
+                        if(tiro.matches("\\d \\d") || tiro.matches("\\d{2} \\d{2}") || tiro.matches("\\d \\d{2}") || tiro.matches("\\d{2} \\d")){	
 				String[] coords = tiro.split(" ");
 				int coordY = Integer.parseInt(coords[0]);
 				int coordX = Integer.parseInt(coords[1]);
 				if(nAcertados[coordY-1][coordX-1] == 0){	
 					if(tabuleiro[coordY-1][coordX-1] != '.'){
 						System.out.println("Acertou um "+tabuleiro[coordY-1][coordX-1]);
+                                                if(tabuleiro[coordY-1][coordX-1] == 'A'){
+                                                    Hidroavioes ++;
+                                                            System.out.println("Afundou um Hidroavião");
+                                                            if(Hidroavioes == 5){
+                                                                System.out.println("Todos os Hidroaviões foram afundados");
+                                                            }
+                                                }else if(tabuleiro[coordY-1][coordX-1] == 'S'){
+                                                    Submarinos ++;
+                                                            System.out.println("Afundou um Submarino");
+                                                            if(Submarinos == 4){
+                                                                System.out.println("Todos os Submarinos foram afundados");
+                                                            }
+                                                }
 					}else{
 						System.out.println("Água!");
 					}
@@ -103,7 +129,9 @@ public static void main(String[] args){
 			}
 		}
 	}
-
+        System.out.println("Você tentou "+vezes+" vezes.");
+        System.out.println("Você acertou "+Hidroavioes+" Hidroaviões.");
+        System.out.println("Você acertou "+Submarinos+" Submarinos.");
 }
 
 }
